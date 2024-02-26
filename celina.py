5a8c9d5544f8e0f9ffa746fb9a30af9450bdc87d
Celina Tromp <celina.am.tromp@gmail.com>
2024-02-26 13:15:34 +0100

    commit 25 feb 13:15

--- celina.py
+++ celina.py
@@ -1,2 +1,8 @@
+import pandas
+
 def mijnmethode():
+    pokemons = pandas.read_csv("Pokemon.csv")
+    print(pokemons)
     return "dit komt uit het bestand celina"
+
+print (mijnmethode()) + str(4+5)
