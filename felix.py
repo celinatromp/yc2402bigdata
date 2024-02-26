96fb9de2df2644e2971b775d989efbe05b0b4b99
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-02-26 13:35:36 +0100

    zoeken op pokemon

--- felix.py
+++ felix.py
@@ -1,7 +1,17 @@
 import pandas
+teller = 0
 
-def mijnmethode():
-    return "dit komt uit het bestand " + str(4+5)
+def mijnmethode(pokemonnaam):
+    global teller
+    pokemons = pandas.read_csv("Pokemon.csv")
+    teller += 1
+    print(teller)
+    print(pokemons)
+    for pok in pokemons["Name"]:
+        if pok == pokemonnaam:
+            return "gevonden"
+
+    return "Pokemon niet gevonden " + str(4+5)
 
 
 # print(mijnmethode())
