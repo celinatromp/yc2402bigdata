214b16b0f746e1bd0a6c84b0ebe81f0a2392e451
Celina Tromp <celina.am.tromp@gmail.com>
2024-03-06 13:11:41 +0100

    gitignore

--- celina.py
+++ celina.py
@@ -1,8 +1,11 @@
 import pandas
+teller = 0
 
 def mijnmethode():
     pokemons = pandas.read_csv("Pokemon.csv")
+    teller += 1
+    print(teller)
     print(pokemons)
     return "dit komt uit het bestand celina"
 
-print (mijnmethode()) + str(4+5)
+print (mijnmethode() + str(4+5))
